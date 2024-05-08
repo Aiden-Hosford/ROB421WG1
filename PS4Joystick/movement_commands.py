e912581ac2d6cfeb25326d7c71aad0a9230fd713
Aiden-Hosford <122062666+Aiden-Hosford@users.noreply.github.com>
2024-05-08 21:45:42 +0000

    Created Movement_commands file

---
+++ PS4Joystick/movement_commands.py
@@ -0,0 +1,43 @@
+from UDPComms import Publisher
+import time 
+
+drive_pub = Publisher(8830)
+
+def activate():
+    drive_pub.send({"L1": 1, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
+
+def move_forward(time):
+    drive_pub.send({"L1": 1, "ly": 1, })
+    time.sleep(time)
+    activate()
+
+def turn_left():
+    drive_pub.send({"L1": 1, "lx": -1, })
+    time.sleep(5)
+    activate()
+
+def turn_right():
+    drive_pub.send({"L1": 1, "lx": 1, })
+    time.sleep(5)
+    activate()
+
+if __name__ == "__main__":
+    activate()
+    move_forward(time=5)
+    turn_left()
+    move_forward(time=3)
+    turn_right()
+    move_forward(time=3)
