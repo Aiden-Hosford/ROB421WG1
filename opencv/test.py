8e22661e11cad56bedfdb60dcd0752f30664ec2c
PreetDhami <108783637+PreetDhami@users.noreply.github.com>
2024-05-16 11:04:34 -0700

    finds midpoint of person in video feed and added psuedocode for turning and shooting

--- opencv/test.py
+++ opencv/test.py
@@ -1,6 +1,7 @@
 import cv2
 import imutils
-  
+import math
+
 # Initializing the HOG person
 # detector
 hog = cv2.HOGDescriptor()
@@ -22,18 +23,35 @@ while cap.isOpened():
                                             winStride=(4, 4),
                                             padding=(4, 4),
                                             scale=1.05)
-        print()
+
+
+
 
 
         # Drawing the regions in the 
         # Image
+        midpoint = (0,0)
         for (x, y, w, h) in regions:
             cv2.rectangle(image, (x, y),
                           (x + w, y + h), 
                           (0, 0, 255), 2)
-  
+            midpoint = (math.floor(x + w/2), math.floor(y + h/2))
+
         # Showing the output Image
         cv2.imshow("Image", image)
+        if len(regions) > 0:
+            #stop robot
+            print("person found, stopping")
+            if midpoint[0] < 190:
+                #turn left
+                print("turn left")
+            elif midpoint[0] > 210:
+                #turn right
+                print("turn right")
+            else: 
+                #shoot
+                print("shooting")
+
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
     else:
