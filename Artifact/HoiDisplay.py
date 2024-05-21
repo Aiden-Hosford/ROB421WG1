33751c396c889998a736640e35f3b464f7c400c5
Hoimau Tan <tanhoimau@gmail.com>
2024-05-21 10:42:35 -0700

    hoi Display

---
+++ Artifact/HoiDisplay.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from MangDang.LCD.ST7789 import ST7789
+
+class Display:
+
+    def __init__(self):
+        self.disp = ST7789()
+        self.disp.begin()
+
+    def show_image(self, image_path):
+        image = Image.open(image_path)
+        image.resize((320, 240))
+        self.disp.display(image)
+
+if __name__ == "__main__":
+    disp = Display()
+    disp.show_image('to_the_moon.jpeg')
