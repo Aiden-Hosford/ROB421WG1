90f25323d3c9bdffed081dcd92ed00852faae111
Hoimau Tan <tanhoimau@gmail.com>
2024-05-21 10:57:31 -0700

    video?

--- Artifact/HoiDisplay.py
+++ Artifact/HoiDisplay.py
@@ -1,3 +1,4 @@
+import cv2
 from PIL import Image
 from MangDang.LCD.ST7789 import ST7789
 
@@ -7,11 +8,21 @@ class Display:
         self.disp = ST7789()
         self.disp.begin()
 
-    def show_image(self, image_path):
-        image = Image.open(image_path)
-        image.resize((320, 240))
+    def show_image(self, image):
+        image = Image.fromarray(image)
+        image = image.resize((320, 240))
         self.disp.display(image)
+
+    def show_video(self, video_path):
+        cap = cv2.VideoCapture(video_path)
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if ret:
+                self.show_image(frame)
+            else:
+                break
+        cap.release()
 
 if __name__ == "__main__":
     disp = Display()
-    disp.show_image('cropped.jpg')
+    disp.show_video('path_to_your_video.mp4')
