b1caa99b33f41b1deec714ffe11efce4bca2173f
PreetDhami <108783637+PreetDhami@users.noreply.github.com>
2024-05-22 13:21:10 -0700

    made timer work now

--- Artifact/preet_artifact.py
+++ Artifact/preet_artifact.py
@@ -13,16 +13,15 @@ if __name__ == "__main__":
     starttime = time.monotonic()
     currenttime = time.monotonic()
     with Image.new("RGB", size=(320, 240)) as im:
-        # while True:
+        while True:
 
-        draw = ImageDraw.Draw(im)
-        draw.text(xy=(160, 120), align="center", text=f"{currenttime-starttime:.2f}")
+            draw = ImageDraw.Draw(im)
+            draw.text(xy=(160, 120), align="center", text=f"{currenttime-starttime:.2f}")
 
-        # write to stdout
-        disp.display(im)
-        # im.paste((0,0,0), (0,0,320,240))
-        print("test")
-        time.sleep(0.05 - ((time.monotonic() - starttime) % 0.05))
+            # write to stdout
+            disp.display(im)
+            im.paste((0,0,0), (0,0,320,240))
+            time.sleep(0.05 - ((time.monotonic() - starttime) % 0.05))
 
     
 
